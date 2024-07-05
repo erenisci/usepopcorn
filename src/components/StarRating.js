6b81f2e1ad2452687f803f8022c558eb012069b9
Eren İşci <isci.eren@gmail.com>
2024-07-06 02:59:57 +0300

    src/components

---
+++ src/components/StarRating.js
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import Star from './Star';
+
+const containerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '16px',
+};
+
+const starContainerStyle = {
+  display: 'flex',
+};
+
+const StarRating = ({
+  maxRating = 5,
+  defaultRating = 0,
+  color = '#fcc419',
+  size = 48,
+  className = '',
+  messages = [],
+  onSetRating,
+}) => {
+  const [rating, setRating] = useState(defaultRating);
+  const [tempRating, setTempRating] = useState(0);
+
+  const handleRating = rate => {
+    setRating(rating => (rating === rate ? 0 : rate));
+    if (onSetRating) onSetRating(rating => (rating === rate ? 0 : rate));
+  };
+
+  const textStyle = {
+    lineHeight: '1',
+    margin: '0',
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
+  return (
+    <div
+      style={containerStyle}
+      className={className}
+    >
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star
+            key={i}
+            full={tempRating ? tempRating > i : rating > i}
+            onRate={() => handleRating(i + 1)}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
+          />
+        ))}
+      </div>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ''}
+      </p>
+    </div>
+  );
+};
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  messages: PropTypes.array,
+  onSetRating: PropTypes.func,
+};
+
+export default StarRating;
