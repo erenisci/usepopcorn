fc8cefcd07a92aa5503b8de015311e248f670548
Eren İşci <isci.eren@gmail.com>
2024-07-14 10:39:33 +0300

    for API_KEY

--- src/components/useMovies.js
+++ src/components/useMovies.js
@@ -14,7 +14,7 @@ export const useMovies = query => {
           setError('');
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
