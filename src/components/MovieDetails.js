270e9b0f67a94c871694a7c7560407c245b3bdcc
Eren İşci <isci.eren@gmail.com>
2024-07-14 10:48:40 +0300

    for API_KEY

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -58,7 +58,7 @@ const MovieDetails = ({ selectedId, onCloseMovie, onAddWatched, watched }) => {
       setIsLoading(true);
 
       const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&i=${selectedId}`
+        `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&i=${selectedId}`
       );
       const data = await res.json();
       setMovie(data);
